9789b908e15985b1ef84b3397a17afcbc9ccdc87
Binit Datta <binitdatta@gmail.com>
2015-10-11 10:12:02 -0500

    Added view results html

--- client/app/viewresults/viewresults.controller.js
+++ client/app/viewresults/viewresults.controller.js
@@ -6,9 +6,22 @@ angular.module('mathsgymnasiamApp')
 	 console.log('userid = '+userid);
     $scope.awesomeThings = [];
 
+  var 	currentuser = Auth.getCurrentUser()._id;
+	  var username = Auth.getCurrentUser().name;
+	  var useremail = Auth.getCurrentUser().email;
+	  var userid = Auth.getCurrentUser()._id;
+	  var grade = Auth.getCurrentUser().grade;
+
+	  	$scope.userid = userid;
+					$scope.username = username;
+					$scope.useremail = useremail;
+		//			console.log('grade in fetch :'+grade);
+					$scope.grade = grade;
+
     $http.get('api/takeatest/users/'+userid).success(function(userresults) {
       $scope.userresults = userresults;
 	  console.log(userresults);
+	  	$scope.studentresults = userresults;
     });
 
   });
